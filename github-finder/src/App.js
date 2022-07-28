9a897d9711e24632fa93e419fd38bad8cd689667
Yogesh, L <yogesh.linganna@gmail.com>
2022-07-28 16:50:40 -0400

    1.Javascript vs JSX way of writing 2. React.Fragment for root node

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
 class App extends Component {
 
+  // should have only one root element.
+  // If root element is not there then we can use <React.Fragment> ...</React.Fragment>
   render(){
+    //JSX way
     return (
       <div className="App">
          <h1>Hello from React</h1>
       </div>
     );
+
+    // Javascript way
+    // return React.createElement
+    // ( 
+    //   'div',
+    //   {className: 'App'}, 
+    //   React.createElement('h1',null,'Hello from React')
+    // );
   }
   
 }
