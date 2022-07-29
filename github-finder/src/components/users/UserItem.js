38cd17c426b18f2875d8a291604a6f2b1061c288
Yogesh, L <yogesh.linganna@gmail.com>
2022-07-29 17:52:26 -0400

    Component state without constructor

--- github-finder/src/components/users/UserItem.js
+++ github-finder/src/components/users/UserItem.js
@@ -2,27 +2,26 @@ import React, { Component } from 'react'
 
 class UserItem extends Component {
 
-    constructor() {
-        super();
-        this.state = {
+    state = {
             id: 'id',
             login: 'mojombo',
             avatar_url:'https://avatars0.githubusercontent.com/u/1?v=4',
             html_url:'https://github.com/mojombo'
         };
-    }
+    
 
     render() {
+        const { login, avatar_url, html_url } = this.state;
         return (
             <div class="card text-center">
                 <img 
-                    src={this.state.avatar_url} 
+                    src={avatar_url} 
                     alt='' 
                     className='round-img' 
                     style={{width: '60px'}} />
-                <h3>{this.state.login}</h3>
+                <h3>{login}</h3>
                 <div>
-                    <a class='btn btn-dark btn-sm my-1' href={this.state.html_url}>More</a>
+                    <a class='btn btn-dark btn-sm my-1' href={html_url}>More</a>
                 </div>
             </div>
         )
