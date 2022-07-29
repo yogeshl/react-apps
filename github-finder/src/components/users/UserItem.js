59e1b969d47260fdc4c79c9817460a48cbe3c16d
Yogesh, L <yogesh.linganna@gmail.com>
2022-07-29 18:05:11 -0400

    1.Lists 2.Passing state with Props 3.Inline styling

--- github-finder/src/components/users/UserItem.js
+++ github-finder/src/components/users/UserItem.js
@@ -2,18 +2,10 @@ import React, { Component } from 'react'
 
 class UserItem extends Component {
 
-    state = {
-            id: 'id',
-            login: 'mojombo',
-            avatar_url:'https://avatars0.githubusercontent.com/u/1?v=4',
-            html_url:'https://github.com/mojombo'
-        };
-    
-
     render() {
-        const { login, avatar_url, html_url } = this.state;
+        const { login, avatar_url, html_url } = this.props.user;
         return (
-            <div class="card text-center">
+            <div className="card text-center">
                 <img 
                     src={avatar_url} 
                     alt='' 
@@ -21,7 +13,7 @@ class UserItem extends Component {
                     style={{width: '60px'}} />
                 <h3>{login}</h3>
                 <div>
-                    <a class='btn btn-dark btn-sm my-1' href={html_url}>More</a>
+                    <a className='btn btn-dark btn-sm my-1' href={html_url}>More</a>
                 </div>
             </div>
         )
