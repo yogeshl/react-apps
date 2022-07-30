d3686c1bdac34582c9b2dc60309f0c8421752d70
Yogesh, L <yogesh.linganna@gmail.com>
2022-07-30 01:30:34 -0400

    Spinner component and Refacto

--- github-finder/src/components/users/Users.js
+++ github-finder/src/components/users/Users.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import UserItem  from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
-class Users extends Component {
+const Users = ({ users, loading}) =>  {
 
-    render() {
+    if(loading){
+        return <Spinner />
+    }else {
         return (
             <div style={userStyle}>
-                {this.props.users.map(user => (
+                {users.map(user => (
                     <UserItem key={user.id} user={user} />
                 ))}
             </div>
         )
     }
+    
 }
 
 
@@ -22,3 +27,8 @@ const userStyle = {
     gridTemplateColumns: 'repeat(3, 1fr',
     gridGap: '1rem'
 };
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
+}
