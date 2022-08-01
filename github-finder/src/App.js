c012885e07896afe6634f7cfde09919184a3c40c
Yogesh, L <yogesh.linganna@gmail.com>
2022-08-01 17:10:39 -0400

    1.Passing props up i.e child to parent 2. Search git hub users 3.Use prop-types in search component

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -21,12 +21,21 @@ class App extends Component {
     
   };
 
+  //Search Github users
+  searchUsers = async (text) =>{
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    
+    this.setState({users: res.data.items, loading: false});
+  }
+
   render(){
     return (
       <div className="App">
          <Navbar />
          <div className='container'>
-            <Search />
+            <Search searchUsers={this.searchUsers} />
             <Users loading={this.state.loading} users={this.state.users} />
          </div>
       </div>
--- github-finder/src/components/users/Search.js
+++ github-finder/src/components/users/Search.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
 
     state ={
         text: ''
     };
+
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired,
+    }
 
     onChange = (e) => {
         this.setState({ [e.target.name]: e.target.value });
@@ -20,7 +25,8 @@ export class Search extends Component {
 
     onSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state.text);
+        this.props.searchUsers(this.state.text);
+        this.setState({text: ''});
     }
 
     render() {
