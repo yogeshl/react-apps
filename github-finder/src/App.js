c7946b8561ec5dd013f3ebc604d95b4c7121f04c
Yogesh, L <yogesh.linganna@gmail.com>
2022-08-08 18:11:24 -0400

    1.Implement Context API using Reducer 2.Implement similar to reducer pattern

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -8,6 +8,10 @@ import Repos from './components/repos/Repos';
 import Search from './components/users/Search';
 import About from './components/Pages/About';
 import axios from 'axios';
+
+
+import GithubState from './context/github/GithubState';
+
 import './App.css';
 
 const App = () => {
@@ -74,51 +78,53 @@ const App = () => {
 
 
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <div className='container'>
-            <Alert alert={alert} />
+      <GithubState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className='container'>
+              <Alert alert={alert} />
 
-            <Routes>
-      
-              <Route exact path="/" element={
-                <Fragment>
+              <Routes>
+        
+                <Route exact path="/" element={
+                  <Fragment>
 
-                  <Search 
-                      searchUsers={searchUsers} 
-                      clearUsers={clearUsers} 
-                      showClear={ users.length > 0 ? true: false } 
-                      setAlert={showAlert} />
+                    <Search 
+                        searchUsers={searchUsers} 
+                        clearUsers={clearUsers} 
+                        showClear={ users.length > 0 ? true: false } 
+                        setAlert={showAlert} />
 
-                  <Users loading={loading} users={users} />
+                    <Users loading={loading} users={users} />
 
-                </Fragment>
-              } />
+                  </Fragment>
+                } />
 
-              <Route exact path="/about" element={ <About /> } />
-              {/* Not Working in React V6 */}
-              {/* <Route exact path="/user/:login" element ={ (props) => 
-                <User 
-                     {...props}
-                      getUser={this.getUser}  
-                      user={user} 
-                      loading={loading} />
-              } /> */}
+                <Route exact path="/about" element={ <About /> } />
+                {/* Not Working in React V6 */}
+                {/* <Route exact path="/user/:login" element ={ (props) => 
+                  <User 
+                      {...props}
+                        getUser={this.getUser}  
+                        user={user} 
+                        loading={loading} />
+                } /> */}
 
-              <Route exact path="/user/:login" element ={ 
-                <User 
-                      getUser={getUser}  
-                      getUserRepos={getUserRepos}
-                      user={user} 
-                      repos={repos}
-                      loading={loading} />
-              } />
-      
-            </Routes>
-          </div>
-      </div>
-      </Router>
+                <Route exact path="/user/:login" element ={ 
+                  <User 
+                        getUser={getUser}  
+                        getUserRepos={getUserRepos}
+                        user={user} 
+                        repos={repos}
+                        loading={loading} />
+                } />
+        
+              </Routes>
+            </div>
+        </div>
+        </Router>
+      </GithubState>
     );
   
 }
