304aeed23bc22dfdfe5028d2bdd51b9950f4b272
Yogesh, L <yogesh.linganna@gmail.com>
2022-08-23 14:35:07 -0400

    Creating Dependencies and basic express setup

---
+++ contact-keeper/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API..'}));
+
+const PORT = process.env.port || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
