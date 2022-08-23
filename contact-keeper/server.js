022664bc293c73eb39a69acded44ce4beca40740
Yogesh, L <yogesh.linganna@gmail.com>
2022-08-23 15:59:50 -0400

    Setting up Backend routes

--- contact-keeper/server.js
+++ contact-keeper/server.js
@@ -6,3 +6,8 @@ app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API..'}
 
 const PORT = process.env.port || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+//Define routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
