a7a0a56bacc4b2f8280206d686dafc51c8448533
Yogesh, L <yogesh.linganna@gmail.com>
2022-08-26 16:58:08 -0400

    Hashing password

--- contact-keeper/routes/users.js
+++ contact-keeper/routes/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const express = require('express');
 const { check, validationResult } = require('express-validator/check');
 const router = express.Router();
@@ -11,13 +12,37 @@ router.post('/',[
     check('name', 'Name is required').not().isEmpty(),
     check('email','Please include a valid email') .isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({min:6})
-], (req, res) => {
+], async (req, res) => {
 
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array() });
     }
-    res.send('Passed..');
+    
+
+    const { name , email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if(user){
+            return res.status(400).json({msg: 'User already exists'});
+        }
+
+        user = new User({name, email, password});
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        return res.send('User saved.')
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+
 }); 
 
 
