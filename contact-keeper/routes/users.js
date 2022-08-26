ac1e2991dd9547765d580f5c43f3db9d04390e52
Yogesh, L <yogesh.linganna@gmail.com>
2022-08-26 17:53:07 -0400

    Auth Route implement

--- contact-keeper/routes/users.js
+++ contact-keeper/routes/users.js
@@ -1,5 +1,5 @@
-const bcrypt = require('bcryptjs');
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator/check');
