2c48dddbb7e53cfb7e27cb89c3e7bf20dda3f791
Yogesh, L <yogesh.linganna@gmail.com>
2022-08-26 16:48:04 -0400

    Update users route and integrate express validators

--- contact-keeper/routes/users.js
+++ contact-keeper/routes/users.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator/check');
 const router = express.Router();
+
+const User = require('../models/User');
 
 //@route POST api/users
 //@desc Register a user
 //@access Public
-router.post('/', (req, res) => {
-    res.send('Rgister a user');
+router.post('/',[ 
+    check('name', 'Name is required').not().isEmpty(),
+    check('email','Please include a valid email') .isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({min:6})
+], (req, res) => {
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array() });
+    }
+    res.send('Passed..');
 }); 
 
 
